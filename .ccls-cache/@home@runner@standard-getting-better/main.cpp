9fc4462409c0d7ff08083025fa1d69135ec6de45
Anmol Arora <anmolarora5300@gmail.com>
2022-02-18 18:44:38 +0000

    Stl libraries vector and algorithm added

--- .ccls-cache/@home@runner@standard-getting-better/main.cpp
+++ .ccls-cache/@home@runner@standard-getting-better/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
-  vector<int> a(500,0);
+  vector<int> z(500,0);
 
-  for (int i:a)
+  for (int i:z)
     cout<<i<<" ";
   cout<<endl;
+
+  int a=5;
+  int b=3;
+  cout<<"a-> "<< a<<" "<<"b-> "<<b<<endl;
+
+  swap(a,b);
+
+  cout<<"a-> "<< a<< " "<<"b-> "<<b<<endl;
+
+  //min
+  cout<<"min -> "<<min(a,b)<<endl;
+  //max
+  cout<<"max -> "<<max(a,b)<<endl;
+  
 }
